c2ab4f807c91a25a1aaa97c91393966bf1761698
Sharujah1 <sharubareth@gmail.com>
2024-12-18 16:41:30 +0100

    javascript code einsetzen

---
+++ javascript/express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dispenssystem'
+});
+
+app.post('/antrag', (req, res) => {
+    const {lernende_id, lehrende_id, datum, grund} = req.body;
+    const query = 'INSERT INTO Dispensationsantraege (lernende_id, lehrende_id, datum, grund, status) VALUES (?, ?, ?, ?, ?)';
+    db.query(query, [lernende_id, lehrende_id, datum, grund, 'Pending'], (err, result) => {
+        if (err) throw err;
+        res.send('Antrag erfolgreich eingereicht');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server läuft auf Port 3000');
+});
